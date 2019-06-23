9ba6e62ef1a12478ed79252bded37ac8df1e163a
Christopher Szatmary <cs@christopherszatmary.com>
2019-06-22 22:04:04 -0400

    Added tests for If and Switch

---
+++ Tests/SwiftySweetnessTests/Classes/SwitchTests.swift
@@ -0,0 +1,49 @@
+//
+//  SwitchTests.swift
+//  SwiftySweetness
+//
+//  Created by Christopher Szatmary on 2019-06-22.
+//
+
+import Quick
+import Nimble
+@testable import SwiftySweetness
+
+final class SwitchTests: QuickSpec {
+    override func spec() {
+        describe("Switch.swift tests") {
+            it("returns the value specified in the first case") {
+                let action = "start"
+                let message = Switch(action)
+                    .case("start") { "Starting..." }
+                    .case("stop") { "Stopping..." }
+                    .case("pause") { "Pausing..." }
+                    .default { "Unknown Action" }
+
+                expect(message).to(equal("Starting..."))
+            }
+
+            it("returns the value specified in the second case") {
+                let action = "stop"
+                let message = Switch(action)
+                    .case("start") { "Starting..." }
+                    .case("stop") { "Stopping..." }
+                    .case("pause") { "Pausing..." }
+                    .default { "Unknown Action" }
+
+                expect(message).to(equal("Stopping..."))
+            }
+
+            it("returns the value specified in the default case") {
+                let action = "naw"
+                let message = Switch(action)
+                    .case("start") { "Starting..." }
+                    .case("stop") { "Stopping..." }
+                    .case("pause") { "Pausing..." }
+                    .default { "Unknown Action" }
+
+                expect(message).to(equal("Unknown Action"))
+            }
+        }
+    }
+}
